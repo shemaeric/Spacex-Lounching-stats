f5a65a4fb439072c5617cd9a84fa0ac9ebd45052
shemaeric <shemaeric01@gmail.com>
2019-09-28 22:50:40 +0200

    add the react app and make the UI

---
+++ app/src/components/LaunchItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classNames from 'classnames';
+
+export default function LaunchItem({
+  launch: { flight_number, mission_name, launch_date_local, launch_success }
+}) {
+  return (
+    <div className="card card-body mb-3">
+      <div className="row">
+        <div className="col-md-9">
+          <h4>Mission: <span className={classNames({
+              'text-success': launch_success,
+              'text-danger': !launch_success
+          })}>{mission_name}</span></h4>
+          <p>Date: {launch_date_local}</p>
+        </div>
+        <div className="col-md-3">
+          <button className="btn btn-secondary"> Launch Details</button>
+        </div>
+      </div>
+    </div>
+  );
+}
